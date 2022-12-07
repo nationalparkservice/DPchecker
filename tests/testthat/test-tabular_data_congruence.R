33e69a107021aad826731af73f2f20fc4d3e4362
Wright <sewright@nps.gov>
2022-12-07 11:24:47 -0700

    Add unit tests

--- tests/testthat/test-tabular_data_congruence.R
+++ tests/testthat/test-tabular_data_congruence.R
@@ -119,10 +119,96 @@ test_that("test_delimiter throws error if metadata indicates delimiter with zero
   expect_error(test_delimiter(load_metadata(here::here(bad_dir, "BICY_bad"))),
                "Metadata indicates that the following data files do not contain valid delimiters:.*Example Intercept Observations.*Example Transect Observations$")
 })
+
 # ---- test_dup_meta_entries ----
-# ---- test_dup_data_files ----
+test_that("test_dup_meta_entries displays success message if no duplicate files listed in metadata", {
+  expect_message(test_dup_meta_entries(load_metadata(here::here(good_dir, "BICY_good"))),
+                 "Each data file name is used exactly once in the metadata file.")
+  expect_message(test_dup_meta_entries(load_metadata(here::here(good_dir, "BUIS_good"))),
+                 "Each data file name is used exactly once in the metadata file.")
+
+})
+
+test_that("test_dup_meta_entries displays error message if metadata contains duplicate filenames", {
+  expect_error(test_dup_meta_entries(load_metadata(here::here(bad_dir,"data_metadata_mismatch", "BICY"))),
+                 "Metadata file name check failed. Some filenames are used more than once in the metadata:.*Mini_BICY_Veg_Transect_Cleaned.csv.*Mini_BICY_Veg_Intercept_Cleaned.csv$")
+  expect_error(test_dup_meta_entries(load_metadata(here::here(bad_dir, "data_metadata_mismatch", "BUIS"))),
+                 "Metadata file name check failed. Some filenames are used more than once in the metadata:.*BUIS_herps.csv$")
+
+})
+
 # ---- test_file_name_match ----
+test_that("test_file_name_match displays success message if files in data dir and metadata match", {
+  expect_message(test_file_name_match(here::here(good_dir, "BICY_good")),
+                 "All data files are listed in metadata and all metadata files names refer to data files.")
+  expect_message(test_file_name_match(here::here(good_dir, "BUIS_good")),
+                 "All data files are listed in metadata and all metadata files names refer to data files.")
+
+})
+
+test_that("test_file_name_match displays error message if metadata contains filenames not in data dir and vice versa", {
+  expect_error(test_file_name_match(here::here(bad_dir,"data_metadata_mismatch", "BICY_files")),
+                 "1 file listed in metadata and missing from data folder.*1 file present in data folder and missing from metadata")
+  expect_error(test_file_name_match(here::here(bad_dir, "data_metadata_mismatch", "BUIS_files")),
+                 "1 file listed in metadata and missing from data folder.*0 files present in data folder and missing from metadata")
+
+})
+
 # ---- test_fields_match ----
+test_that("test_fields_match displays success message if columns in data dir and metadata match", {
+  expect_message(test_fields_match(here::here(good_dir, "BICY_good")),
+                 "All columns in data match all columns in metadata")
+  expect_message(test_fields_match(here::here(good_dir, "BUIS_good")),
+                 "All columns in data match all columns in metadata")
+
+})
+
+test_that("test_fields_match displays error message if columns are missing from data/metadata or in wrong order", {
+  expect_error(test_fields_match(here::here(bad_dir,"data_metadata_mismatch", "BICY_columns")),
+               "Column mismatch between data and metadata\\W*Mini_BICY_Veg_Geography\\.csv\\W*Missing from metadata: decimalLatitude\\W*Mini_BICY_Veg_Intercept_Cleaned\\.csv\\W*Missing from data file: vernacularName\\W*Mini_BICY_Veg_Transect_Cleaned\\.csv\\W*: Metadata column order does not match data column order$")
+  expect_error(test_fields_match(here::here(bad_dir, "data_metadata_mismatch", "BUIS_columns")),
+               "Column mismatch between data and metadata.*BUIS_herps.csv.*Missing from metadata.*genus.*Missing from data file.*family")
+
+})
+
+
 # ---- test_numeric_fields ----
+test_that("test_numeric_fields displays success message if columns reported as numeric in the metadata can be parsed as numbers", {
+  expect_message(test_numeric_fields(here::here(good_dir, "BICY_good")),
+                 "Columns indicated as numeric in metadata contain only numeric values and valid missing value codes")
+  expect_message(test_numeric_fields(here::here(good_dir, "BUIS_good")),
+                 "Columns indicated as numeric in metadata contain only numeric values and valid missing value codes")
+
+})
+
+test_that("test_numeric_fields displays error message if columns reported as numeric in metadata cannot be parsed as numbers", {
+  expect_error(test_numeric_fields(here::here(bad_dir,"bad_data_types", "BICY")),
+               "Columns indicated as numeric in metadata contain non-numeric values:\\W*Mini_BICY_Veg_Intercept_Cleaned.csv\\W*: custom_Transect\\W*Mini_BICY_Veg_Transect_Cleaned.csv\\W*: individualCount$")
+  expect_error(test_numeric_fields(here::here(bad_dir, "bad_data_types", "BUIS")),
+               "Columns indicated as numeric in metadata contain non-numeric values:\\W*BUIS_herps.csv\\W*: coordinateUncertaintyInMeters$")
+})
+
 # ---- test_date_range ----
+test_that("test_date_range displays success message if dates in data match temporal coverage in metadata", {
+  expect_message(test_date_range(here::here(good_dir, "BICY_good")),
+                 "Columns indicated as date/time in metadata are within the stated temporal coverage range")
+  expect_message(test_date_range(here::here(good_dir, "BUIS_good")),
+                 "Columns indicated as date/time in metadata are within the stated temporal coverage range.")
+
+})
+
+test_that("test_date_range displays error message if dates in data fail to parse", {
+  expect_error(test_date_range(here::here(bad_dir,"bad_data_types", "BICY")),
+               "The following date/time columns are out of the range \\W*2002-04-08\\W*2006-06-24\\W* specified in the metadata:\\W*Mini_BICY_Veg_Intercept_Cleaned.csv\\W*: eventDate \\(partially failed to parse\\)\\W*Mini_BICY_Veg_Transect_Cleaned.csv\\W*: eventDate \\(failed to parse\\)$")
+})
+
+test_that("test_date_range displays warning message if dates in data are outside temporal coverage range", {
+  expect_warning(test_date_range(here::here(bad_dir,"bad_data_types", "BUIS")),
+               "The following date/time columns are out of the range \\W*2001-06-20\\W*2001-10-20\\W* specified in the metadata:\\W*BUIS_herps.csv\\W*: eventDate \\W*2001-06-20\\W*2020-10-18\\W*$")
+})
+
 # ---- convert_datetime_format ----
+test_that("convert_datetime_format returns the correct R datetime format string for ISO compliant date formats", {
+  expect_equal(convert_datetime_format("YYYY-MM-DD"), "%Y-%m-%d")
+  expect_equal(convert_datetime_format("YYYY-MM-DDThh:mm:ss"), "%Y-%m-%dT%H:%M:%S")
+})
