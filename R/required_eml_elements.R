44d7cdfaec58e3873178b767aed6453481a8e934
Rob Baker <robert_baker@nps.gov>
2023-06-20 15:36:04 -0600

    stray # in documentation

--- R/required_eml_elements.R
+++ R/required_eml_elements.R
@@ -2,7 +2,8 @@
 #'
 #' @description `test_pub_date()` tests for presence and ISO-8601 formatting of the publication date. Also tests whether the publication date is within a reasonable bounds; i.e. not a date prior to the beginning of the data package Reference Type (after 2022) and not in a future year. If the publication date is missing or improperly formatted, the test fails with an error. If the publication date is outside a reasonable range, the test fails with a warning. Otherwiset the test passes.
 #'
-#' @param metadata The metadata object returned by `load_metadata`. If parameter is not provided, it defaults to calling `load_metadata` in the current project directory.#'
+#' @param metadata The metadata object returned by `load_metadata`. If parameter is not provided, it defaults to calling `load_metadata` in the current project directory.
+#'
 #' @return invisibly returns `metadata`
 #' @export
 #'
