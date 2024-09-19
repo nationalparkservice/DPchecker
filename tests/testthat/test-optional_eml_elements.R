85de26330ce10f2572bb2bb970062c2344fe5d43
Rob Baker <robert_baker@nps.gov>
2024-09-19 09:27:08 -0600

    add minimal unit tests for each function in optional_eml_elements.R

--- tests/testthat/test-optional_eml_elements.R
+++ tests/testthat/test-optional_eml_elements.R
@@ -3,24 +3,90 @@ bad_dir <- test_path("bad")
 bicy_meta <- load_metadata(test_path("good", "BICY_good"))
 buis_meta <- load_metadata(test_path("good", "BUIS_good"))
 
-# ---- load_metadata ----
-test_that("load_metadata works on valid EML file", {
-  expect_message(load_metadata(here::here(good_dir, "BICY_good"), inform_success = TRUE),
-                 ".*Metadata check passed.*")
-  expect_message(load_metadata(here::here(good_dir, "BUIS_good"), inform_success = TRUE),
-                 ".*Metadata check passed.*")
-})
-
 # ---- test_pii_meta_emails
 
-test_that("test_pii_meta_emails finds true negatives", {
-  expect_message(test_pii_meta_emails(here::here(good_dir, "BICY_good")),  "Metadata does not appear to contain any personal emails")
-})
+test_that("test_pii_meta_emails finds true negatives",
+          {
+            msg <- "Metadata does not appear to contain any personal emails"
+            expect_message(test_pii_meta_emails(here::here(good_dir,
+                                                           "BICY_good")),
+                           msg)
+            }
+          )
 
 # ---- test_notes
-test_that("test_notes correctly warns when no notes present", {
-  expect_warning(test_notes(metadata = bicy_meta), "Metadata does not contain additionalInfo*")
-})
+test_that("test_notes correctly warns when no notes present",
+          {
+            expect_warning(test_notes(metadata = bicy_meta),
+                           "Metadata does not contain additionalInfo*")
+            }
+          )
 
 
 # ---- test_methods
+
+test_that("test_methods identifies presence of non-standard characters",
+          {
+            msg <- paste0("The metadata methods contains ",
+                          "non-standard characters such as*")
+            expect_warning(test_methods(metadata = bicy_meta),
+                           msg)
+            }
+          )
+
+# ---- test_orcid_exists
+
+test_that("test_orcid_exists correctly identifies orcids exist",
+          {
+            expect_message(test_orcid_exists(metadata = bicy_meta),
+                           "All individual creators have associated ORCiDs.")
+})
+
+# ---- test_orcid_format
+
+test_that("test_orcid_format identifies properly formatted orcids",
+          {
+            expect_message(test_orcid_format(metadata = bicy_meta),
+                           "All Creator ORCiDs are properly formatted.")
+            }
+          )
+
+# ---- test_orcid_resolves
+
+# ----- test_orcid_resolves ----
+test_that("test_orcid_resolves correctly identifies orcids that resolve",
+          {
+            msg <- "All Creator ORCiDs resolved to a valid ORCiD profile."
+            expect_message(test_orcid_resolves(metadata = bicy_meta),
+                           msg)
+            }
+          )
+
+# ---- test_orcid_match
+test_that("test_orcid_match correctly identifies when orcid and crearot match",
+         {
+           msg <- paste0("All Creator ORCiDs resolve to an ORCiD profile ",
+                         "that matches the Creator last name.")
+           expect_message(test_orcid_match(metadata = bicy_meta),
+                          msg)
+           }
+         )
+
+# ---- test_public_points
+test_that("test_public_points warns GPS points exist in restricted metadata",
+          {
+            msg <- paste0("CUI is not set to PUBLIC. GPS coordinates detected ",
+                          "in metadata will be publicly available. ",
+                          "Are you sure?")
+            expect_warning(test_public_points(metadata = bicy_meta),
+                           msg)
+            }
+          )
+
+# ---- test_project
+test_that("test_project warns when no project is present",
+          {
+            expect_warning(test_project(metadata = bicy_meta),
+                           "No project associated with the metadata.*")
+            }
+          )
