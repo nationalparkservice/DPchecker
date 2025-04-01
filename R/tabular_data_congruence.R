8e01a585a77e1cc907906850212cb349d2d8d909
Robert Baker <robert_baker@nps.gov>
2025-04-01 13:45:51 -0600

    update test_datatable_urls_doi to warn if ezEML inserted function = "download"

--- R/tabular_data_congruence.R
+++ R/tabular_data_congruence.R
@@ -431,10 +431,15 @@ test_datatable_urls_doi <-  function (metadata = load_metadata(directory)) {
         cli::cli_abort(c("x" = "One or more data files lack URLs. Could not test whether URLs are properly formatted or correspond to the corect DOI. Use {.fn EMLeditor::set_data_urls} to add them."))
         return(invisible(metadata))
       }
+
       #handle <url function="download> tag from ezEML:
       if (length(seq_along(url)) > 1) {
-        url <- url$url
+        bad_url <- bad_url + 1
+        tbl_name <- data_tbl[[i]][["physical"]][["objectName"]]
+        cli::cli_warn(c("!" = "Metadata URL for the data table {.var {tbl_name}} is incorrectly formatted. Use {.fn EMLeditor::set_data_urls} to update the URLs."))
+        return(invisible(metadata))
       }
+
       prefix <- stringr::str_sub(url, 1, stringr::str_length(url)-7)
       suffix <- stringr::str_sub(url, -7, -1)
 
